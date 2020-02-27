5cc402703b11cc6507d63acec213d33f20f0cc44
felipecespedes <felipecespedespisso@gmail.com>
2020-02-27 16:29:09 -0500

    replaces text content in all nodes

--- src/jota.templates.js
+++ src/jota.templates.js
@@ -8,13 +8,43 @@
 
 (function() {
 
-  // TODO it should work across all node levels
+  function isTextNode(node) {
+    return node.nodeType === 3;
+  }
+
+  function getAllChildren(element) {
+    // TODO review
+    // It works but...
+    return element.getElementsByTagName('*');
+  }
+
+  function extractText(node) {
+    return node.textContent;
+  }
+
+  function replacePropsFor(element, props, key) {
+    [].forEach.call(element.childNodes, function(node) {
+      if (isTextNode(node)) {
+        const regex = new RegExp(`\\{\\{\\s*?${key}\\s*?\\}\\}`, 'gm');
+        const value = props[key];
+        const nodeText = extractText(node);
+        const newTextContent = nodeText.replace(regex, value);
+        node.textContent = newTextContent;
+      }
+    });
+  }
+
   function replaceProps(element, initialElement, props) {
+    const newElement = initialElement.cloneNode(true);
+    const children = getAllChildren(newElement);
     for (key in props) {
-      const regex = new RegExp(`\\{\\{\\s*?${key}\\s*?\\}\\}`, 'gm');
-      const value = props[key];
-      element.innerText = initialElement.innerText.replace(regex, value);
+      replacePropsFor(newElement, props, key);
+      [].forEach.call(children, function(node) {
+        replacePropsFor(node, props, key)
+      });
     }
+    // TODO find a better way to replace content since this is not the best way to do it
+    element.innerHTML = newElement.innerHTML;
   }
 
   function templates(context) {
